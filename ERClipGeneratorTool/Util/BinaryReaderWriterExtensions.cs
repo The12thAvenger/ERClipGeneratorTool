07d749b70f9db21fbf948175c8380bfb98fbba3d
The12thAvenger <85712985+The12thAvenger@users.noreply.github.com>
2023-03-14 20:37:48 +0100

    Integrated necessary files from SAP due to target framework mismatch, switched to SF Elden Ring branch

---
+++ ERClipGeneratorTool/Util/BinaryReaderWriterExtensions.cs
@@ -0,0 +1,18 @@
+﻿using SoulsFormats;
+
+namespace ERClipGeneratorTool.Util;
+
+// Taken from SoulsAssetPipeline by Meowmaritus (https://github.com/Meowmaritus/SoulsAssetPipeline/)
+public static class BinaryReaderWriterExtensions
+{
+    public static long GetNextPaddedOffsetAfterCurrentField(this BinaryReaderEx br, int currentFieldLength, int align)
+    {
+        long pos = br.Position;
+        pos += currentFieldLength;
+        if (align <= 0)
+            return pos;
+        if (pos % align > 0)
+            pos += align - (pos % align);
+        return pos;
+    }
+}
